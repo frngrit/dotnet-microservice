22374b87aa16638c37899a16ff794be4b6e6a4e3
frngrit <frank.12876@hotmail.com>
2023-09-02 13:47:24 +0700

    feat: add catalog API

--- src/Catalog.API/Program.cs
+++ src/Catalog.API/Program.cs
@@ -1,4 +1,7 @@
 ﻿
+using Catalog.API.Data;
+using Catalog.API.Entities.Repositories;
+
 namespace Catalog.API;
 
 public class Program
@@ -13,6 +16,11 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        //register repo
+        builder.Services.AddTransient<IProductRepository, ProductRepository>();
+        //register context
+        builder.Services.AddTransient<ICatalogContext, CatalogContext>();
 
         var app = builder.Build();
 
