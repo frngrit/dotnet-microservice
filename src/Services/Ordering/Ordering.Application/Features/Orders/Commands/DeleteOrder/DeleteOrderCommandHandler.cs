78accd5b0fa35128e7115f032ff5f05d03306cb2
rerngrit-frank <frank.12876@hotmail.com>
2023-11-06 21:16:00 +0700

    feat: add deleteOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private IOrderRepository _orderRepository;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _orderRepository.GetByIdAsync(request.Id);
+            await _orderRepository.DeleteAsync(entity);
+
+            return Unit.Value;
+        }
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+	public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+	{
+		public DeleteOrderCommandValidator()
+		{
+			RuleFor(p => p.Id)
+				.NotNull()
+				.NotEmpty()
+				.WithMessage("Id should not be null or empty.");
+		}
+	}
+}
+
